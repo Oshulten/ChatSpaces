d154ef215425515a19f8a6dd9e36e27d7ed6b577
Oscar Hultén <oshulten@gmail.com>
2024-09-15 19:25:01 +0200

    feat: amended Dtos

--- Backend/Dto/DtoUser.cs
+++ Backend/Dto/DtoUser.cs
@@ -2,11 +2,23 @@ using Backend.Models;
 
 namespace Backend.Dto;
 
-public class DtoUser(string id, string username)
+public class DtoUser(string id, string username, List<Guid> spaceGuids, List<Guid> messageGuids)
 {
     public string Id { get; set; } = id;
     public string Username { get; set; } = username;
+    public List<Guid> SpaceGuids { get; set; } = spaceGuids;
+
+    public List<Guid> MessageGuids { get; set; } = messageGuids;
+
 
     public static explicit operator DtoUser(User user) =>
-        new(user.Id, user.Username);
+        new(
+            user.Id,
+            user.Username,
+            user.Messages
+                .Select(message => message.Guid)
+                .ToList(),
+            user.Spaces
+                .Select(space => space.Guid)
+                .ToList());
 }
